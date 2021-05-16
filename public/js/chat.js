a2306ea6fd197ffebbb2b1ae409b6058af268d2a
Aditya Gupta <guptaaditya1108@gmail.com>
2021-05-17 01:19:57 +0530

    Adding Disable Functionality

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,16 +1,29 @@
 //Our own client side JS file
 const socket = io() //connecting to the server!!
 
+
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+
+
 socket.on('message',(greet) =>{
     console.log(greet)
 })
 
-document.querySelector('#message-form').addEventListener('submit',(e) => {
+$messageForm.addEventListener('submit',(e) => {
 
-    e.preventDefault(); //to prevent a full page refresh.
+    e.preventDefault(); //To prevent a full page refresh.
+    $messageFormButton.setAttribute('disabled', 'disabled') //Disabling till the message is delivered
+
     const message = e.target.elements.message.value //e then target (which is form here) then the name of the input--message variable.. and then its value..
 
     socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
         if (error) {
             return console.log(error)
         }
@@ -19,18 +32,21 @@ document.querySelector('#message-form').addEventListener('submit',(e) => {
     })
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
    // navigator.geolocation -: Returns a Geolocation object allowing accessing the location of the device
    
     if (!navigator.geolocation) { //if geolocation is not supported in user's broser
         return alert('Geolocation is not supported by your browser.')
     }
 
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', { //position is an object from which we will access the lat and long 
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            $sendLocationButton.removeAttribute('disabled')
             console.log('Location shared!')  
         })
     })
